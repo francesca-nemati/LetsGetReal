166b44ae95e0d4e02d3e194e1bcb317f0c70aeac
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-11-19 14:23:04 -0500

    first commit - started repo and copied given constructor and methods into file

---
+++ RealNumber.java
@@ -0,0 +1,15 @@
+public class RealNumber {
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+
+}
