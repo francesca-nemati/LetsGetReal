07026eda8385bd23e32f7dba32a7caea1030f95b
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-11-20 19:39:52 -0500

    made multiply method and tested

--- TestRealNumber.java
+++ TestRealNumber.java
@@ -8,12 +8,16 @@ public class TestRealNumber {
     RealNumber num4 = new RealNumber(100.000000002);
 
     RealNumber num5 = new RealNumber(Math.PI);
-    RealNumber num6 = new RealNumber(3.1415892);
+    RealNumber num6 = new RealNumber(3.1415926);
 
     System.out.println(num1.equals(num2));
     System.out.println(num3.equals(num4));
     System.out.println(num5.equals(num6));
 
     System.out.println(num1.add(num2) + ": should be 199;");
+    System.out.println(num5.add(num1) + ": should be 103.1415926;");
+
+    System.out.println(num1.multiply(num2) + ": should be 9900;");
+    System.out.println(num5.multiply(num1) + ": should be 314.15926;");
   }
 }
