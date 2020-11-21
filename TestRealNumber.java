e06d7a2b25ae2bad04ebd6ccef68406dc2186b91
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-11-20 19:34:56 -0500

    made and tested add method

--- TestRealNumber.java
+++ TestRealNumber.java
@@ -7,14 +7,13 @@ public class TestRealNumber {
     RealNumber num3 = new RealNumber(100.000000001);
     RealNumber num4 = new RealNumber(100.000000002);
 
-    System.out.println(num1.toString());
-    System.out.println(num2.toString());
+    RealNumber num5 = new RealNumber(Math.PI);
+    RealNumber num6 = new RealNumber(3.1415892);
 
     System.out.println(num1.equals(num2));
-    System.out.println(num2.equals(num1));
-
     System.out.println(num3.equals(num4));
-    System.out.println(num4.equals(num3));
+    System.out.println(num5.equals(num6));
 
+    System.out.println(num1.add(num2) + ": should be 199;");
   }
 }
